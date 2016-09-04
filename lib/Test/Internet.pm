8e1a6b01d5b072439883cf708814cbeb2b0ee293
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2016-09-04 21:54:32 +0100

    - Added unit test script t/00-load.t. - Updated copyright year information.

--- lib/Test/Internet.pm
+++ lib/Test/Internet.pm
@@ -1,6 +1,6 @@
 package Test::Internet;
 
-$Test::Internet::VERSION   = '0.05';
+$Test::Internet::VERSION   = '0.06';
 $Test::Internet::AUTHORITY = 'cpan:MANWAR';
 
 =head1 NAME
@@ -9,7 +9,7 @@ Test::Internet - Interface to test internet connection.
 
 =head1 VERSION
 
-Version 0.05
+Version 0.06
 
 =cut
 
@@ -108,7 +108,7 @@ Mohammad S Anwar, C<< <mohammad.anwar at yahoo.com> >>
 
 =head1 REPOSITORY
 
-L<https://github.com/Manwar/Test-Internet>
+L<https://github.com/manwar/Test-Internet>
 
 =head1 ACKNOWLEDGEMENT
 
@@ -155,7 +155,7 @@ L<http://search.cpan.org/dist/Test-Internet/>
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright (C) 2015 Mohammad S Anwar.
+Copyright (C) 2015 - 2016 Mohammad S Anwar.
 
 This  program  is  free software;  you can redistribute it and/or modify it under
 the  terms  of the the Artistic  License (2.0). You may obtain a copy of the full
